66e7ab31ce2fc4bae135159d9f66d1a6de8b464f
lgtma <lgtma@live.com>
2015-12-06 00:51:21 +0700

    fd#1

--- scripts.js
+++ scripts.js
@@ -6,7 +6,16 @@
 	$(window).on('load resize',function(){
 		var boxWidth  = $('.box .figure').width();
 		var boxHeight = Math.floor(boxWidth/2);
-		$('.box .figure').css('height',boxHeight);
+		$('.listing .box').each(function(){
+			$('.figure',this).css('height',boxHeight);
+			// var imgWidth = $('.figure.image img',this).width();
+			// var imgHeight = $('.figure.image img',this).height();
+			// // (imgHeight/imgWidth)*newImgWidth = newImgHeight
+			// var newImgHeight = Math.floor(imgHeight/imgWidth)*boxWidth;
+			// $('.figure.image img',this).css({
+			// 	'height':boxHeight
+			// });
+		});
 	});
 	
 })(jQuery);
